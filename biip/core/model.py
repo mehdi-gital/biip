35b792e3ecd87de8cca26413f6797a5436a94861
Amirhossein Nouranizadeh <amirhossein.nouranizadeh@gmail.com>
2022-06-04 17:22:52 +0430

    - variable names changed

--- biip/core/model.py
+++ biip/core/model.py
@@ -17,52 +17,52 @@ class DiffOp(nn.Module):
         self.mp3 = SAGEConv(in_channels=hidden_dim, out_channels=hidden_dim)
         self.mp4 = SAGEConv(in_channels=hidden_dim, out_channels=hidden_dim)
         self.mp5 = SAGEConv(in_channels=hidden_dim, out_channels=input_dim)
-        if activation_fn == 'softmax':
-            self.act = nn.Softmax()
+        if activation_fn == 'softplus':
+            self.act = nn.Softplus()
         else:
             raise NotImplementedError
 
     def forward(self, t, state):
         # unpacking the state
-        f0, regular_edge_index, boundary_values, half_edge_index, timestamps = state
+        f0_interior, regular_edge_index, f_boundary, half_edge_index, timestamps = state
         regular_edge_index = regular_edge_index.long()
         half_edge_index = half_edge_index.long()
-        num_nodes_interior = f0.size(1)
+        num_nodes_interior = f0_interior.size(1)
 
         # linear interpolation on the boundary nodes and between consecutive timestamps
-        boundary_values_t = self.boundary_value_function(t, timestamps, boundary_values)
+        f_boundary_t = self.interpolate_f_boundary(t, timestamps, f_boundary)
 
         # creating edge_index of $G=intG \cup \partial G$
         edge_index = torch.cat([regular_edge_index, half_edge_index], dim=1)
 
         # creating the initial scalar field of $G=intG \cup \partial G$
-        x0 = torch.cat([f0, boundary_values_t], dim=1)
+        x0 = torch.cat([f0_interior, f_boundary_t], dim=1)
 
         # message from the boundary nodes
-        transformed_boundary_values_t = self.gc1.lin_l(boundary_values_t)
+        transformed_f_boundary_t = self.mp1.lin_l(f_boundary_t)
 
         # message passing layer 1 defined in Equation 17
-        x1 = self.gc1(x0, edge_index)
+        x1 = self.mp1(x0, edge_index)
         x1 = self.act(x1)
-        x1 = torch.cat([x1[:, :num_nodes_interior, :], transformed_boundary_values_t], dim=1)  # teacher forcing
+        x1 = torch.cat([x1[:, :num_nodes_interior, :], transformed_f_boundary_t], dim=1)  # teacher forcing
 
         # message passing layer 2 defined in Equation 17
-        x2 = self.gc2(x1, edge_index)
+        x2 = self.mp2(x1, edge_index)
         x2 = self.act(x2)
-        x2 = torch.cat([x2[:, :num_nodes_interior, :], transformed_boundary_values_t], dim=1)  # teacher forcing
+        x2 = torch.cat([x2[:, :num_nodes_interior, :], transformed_f_boundary_t], dim=1)  # teacher forcing
 
         # message passing layer 3 defined in Equation 17
-        x3 = self.gc3(x2, edge_index)
+        x3 = self.mp3(x2, edge_index)
         x3 = self.act(x3)
-        x3 = torch.cat([x3[:, :num_nodes_interior, :], transformed_boundary_values_t], dim=1)  # teacher forcing
+        x3 = torch.cat([x3[:, :num_nodes_interior, :], transformed_f_boundary_t], dim=1)  # teacher forcing
 
         # message passing layer 4 defined in Equation 17
-        x4 = self.gc4(x3, edge_index)
+        x4 = self.mp4(x3, edge_index)
         x4 = self.act(x4)
-        x4 = torch.cat([x4[:, :num_nodes_interior, :], transformed_boundary_values_t], dim=1)  # teacher forcing
+        x4 = torch.cat([x4[:, :num_nodes_interior, :], transformed_f_boundary_t], dim=1)  # teacher forcing
 
         # message passing layer 5 with identity activation
-        x5 = self.gc5(x4, edge_index)
+        x5 = self.mp5(x4, edge_index)
 
         # creating the time derivative state of intG
         dstate_dt = (x5[:, :num_nodes_interior, :], torch.zeros_like(state[1]), torch.zeros_like(state[2]),
@@ -71,21 +71,21 @@ class DiffOp(nn.Module):
         return dstate_dt
 
     @staticmethod
-    def boundary_value_function(t, timestamps, boundary_values):
+    def interpolate_f_boundary(t, timestamps, f_boundary):
         """
         Linearly approximating boundary_value_t based on t, delta_t, boundary_values.
         """
         delta_t = timestamps[1] - timestamps[0]
         k = torch.div(t, delta_t, rounding_mode='floor').long()
         if k == timestamps.size(0) - 1:
-            return boundary_values[k]
+            return f_boundary[k]
         min_time = timestamps[k]
         max_time = timestamps[k + 1]
         # scaling t to 0<t<1
         scaled_t = (t - min_time) / (max_time - min_time)
         # linear interpolation of boundary_values_t
-        boundary_values_t = (1 - scaled_t) * boundary_values[k] + scaled_t * boundary_values[k + 1]
-        return boundary_values_t
+        f_boundary_t = (1 - scaled_t) * f_boundary[k] + scaled_t * f_boundary[k + 1]
+        return f_boundary_t
 
     def __repr__(self):
         return self.__class__.__name__
@@ -103,9 +103,9 @@ class IntOp(nn.Module):
         else:
             self.odeint = odeint
 
-    def forward(self, timestamps, f0, regular_edge_index, boundary_values, half_edge_index):
+    def forward(self, timestamps, f0_interior, regular_edge_index, f_boundary, half_edge_index):
         # packing the state
-        initial_state = (f0, regular_edge_index, boundary_values, half_edge_index, timestamps)
+        initial_state = (f0_interior, regular_edge_index, f_boundary, half_edge_index, timestamps)
 
         # integration
         updated_state = self.odeint(
@@ -133,8 +133,8 @@ class NeuralBIIP(nn.Module):
         self.diffop = DiffOp(input_dim=input_dim, hidden_dim=hidden_dim, activation_fn=activation_fn)
         self.intop = IntOp(diffop=self.diffop, use_adjoint=use_adjoint)
 
-    def forward(self, timestamps, f0, regular_edge_index, boundary_values, half_edge_index):
-        f_t = self.intop(timestamps, f0, regular_edge_index, boundary_values, half_edge_index)
+    def forward(self, timestamps, f0_interior, regular_edge_index, f_boundary, half_edge_index):
+        f_t = self.intop(timestamps, f0_interior, regular_edge_index, f_boundary, half_edge_index)
         return f_t
 
     def __repr__(self):
