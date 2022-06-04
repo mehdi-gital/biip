86491c9ed855b283a46a35eb29f885029e89c3e5
Amirhossein Nouranizadeh <amirhossein.nouranizadeh@gmail.com>
2022-06-05 01:51:02 +0430

    - logger added. - training predictions and plots added

--- train.py
+++ train.py
@@ -1,22 +1,25 @@
-# reads data, loads models, loads configs, trains and creates model artifacts
 import os
 import json
 from biip.core.learner import Learner
 from biip.utils.data import prepare_dataset
+from biip.utils.helpers import get_logger
+
+# paths
+project_name = 'experiment_0'
+data_path = os.path.join('data', project_name)
+artifacts_path = os.path.join('artifacts', project_name)
 
 
 def main():
-    # paths
-    project_name = 'experiment_0'
-    data_path = os.path.join('data', project_name)
-    artifacts_path = os.path.join('artifacts', project_name)
+    # getting logger
+    logger = get_logger(log_path=artifacts_path, prefix='train/logs')
 
-    # reading the configurations
+    # reading configurations
     with open(os.path.join(data_path, 'configs.json')) as file:
         configs = json.load(file)
 
     # constructing the dataset
-    train_dataset = prepare_dataset(data_path=data_path, mode='train')
+    train_dataset = prepare_dataset(data_path=data_path, prefix='train')
 
     # initialize the learner
     learner = Learner(
@@ -27,7 +30,8 @@ def main():
         learning_rate=configs['training']['learning_rate'],
         weight_decay=configs['training']['weight_decay'],
         device=configs['training']['device'],
-        artifacts_path=artifacts_path
+        artifacts_path=artifacts_path,
+        logger=logger
     )
     # fit learner
     learner.fit(
@@ -35,6 +39,15 @@ def main():
         epochs=configs['training']['epochs'],
         batch_size=configs['training']['batch_size'],
         batch_time=configs['training']['batch_time']
+    )
+    # plot learning curves
+    learner.plot_loss_curves()
+
+    # validate model on entire train data
+    learner.predict_train(
+        dataset=train_dataset,
+        grid_size_i=configs['dataset']['grid_size_i'],
+        grid_size_j=configs['dataset']['grid_size_j']
     )
 
 
